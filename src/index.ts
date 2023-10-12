b5b9e4c2ef6513b15f1a73aa8fa96e9f2c9f5d0b
Vitor Diniz <dinizadroit@gmail.com>
2023-10-12 08:59:19 -0300

    lint config

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 3333;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`);
 });
