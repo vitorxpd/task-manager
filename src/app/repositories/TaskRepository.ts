45166c8aa8dfcc0b7ed0142148a0ff43773d2b9d
Vitor Diniz <dinizadroit@gmail.com>
2023-10-12 12:11:09 -0300

    increment tasks

--- src/app/repositories/TaskRepository.ts
+++ src/app/repositories/TaskRepository.ts
@@ -1,7 +1,23 @@
 const tasks = [
   {
     id: '1',
-    text: 'Oiteste',
+    text: 'Fazer compras',
+  },
+  {
+    id: '2',
+    text: 'Estudar',
+  },
+  {
+    id: '3',
+    text: 'Ir ao cinema',
+  },
+  {
+    id: '4',
+    text: 'Jogar videogame',
+  },
+  {
+    id: '5',
+    text: 'Ler livros',
   },
 ];
 
