5af15c56c787e12eaedb9ef6576ed3e830e775dd
Vitor Diniz <dinizadroit@gmail.com>
2023-10-13 13:29:34 -0300

    Fix cors

--- src/app/middlewares/cors.ts
+++ src/app/middlewares/cors.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 
 export function cors(request: Request, response: Response, next: NextFunction) {
   response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
 
   next();
 }
